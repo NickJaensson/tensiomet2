4a214d55f248d7ea61606390b856f7ef9d780dab
NickJaensson <n.jaensson@gmail.com>
2023-12-30 15:03:06 +0100

    Update guess_shape.m

--- src/guess_shape.m
+++ src/guess_shape.m
@@ -1,12 +1,12 @@
-function [r,z,s] = guess_shape(params,N_guess)
+function [r,z,s] = guess_shape(params_phys,N_guess)
 
     % find the initial guess of the droplet shape
-    if  params.Wo > 0.14
+    if  params_phys.Wo > 0.14
     
         % predict the droplet shape using the emperical approach from Nagel
         
         % predict the maximum length of the interface (empirical Nagel)
-        sigmaprime = params.sigma/(params.deltarho*params.grav*params.rneedle^2);
+        sigmaprime = params_phys.sigma/(params_phys.deltarho*params_phys.grav*params_phys.rneedle^2);
 
         smax = sqrt(sigmaprime)*2.0/0.8701;    
     
@@ -18,7 +18,7 @@ function [r,z,s] = guess_shape(params,N_guess)
         r = 4/3*smax/pi*(sin(pi*3/4*s/smax));
 
         % scale the shape to match the radius
-        scale = params.rneedle/r(end);
+        scale = params_phys.rneedle/r(end);
         r = scale*r;
         z = scale*z;
     
@@ -29,9 +29,9 @@ function [r,z,s] = guess_shape(params,N_guess)
     
         % find the initial guess of the droplet shape
         N_guess = 1000;
-        params.rneedle = params.rneedle; 
-        r = linspace(0,params.rneedle,N_guess);
-        z = -sqrt(2*params.volume0/(pi*params.rneedle))*cos(pi*r/(2*params.rneedle));
+        params_phys.rneedle = params_phys.rneedle; 
+        r = linspace(0,params_phys.rneedle,N_guess);
+        z = -sqrt(2*params_phys.volume0/(pi*params_phys.rneedle))*cos(pi*r/(2*params_phys.rneedle));
     
         % determine the curve length
         ds = zeros(size(r));
