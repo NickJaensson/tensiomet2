59e1278ffc8cc6f0b4498d807106c68fe3f1d2d5
NickJaensson <n.jaensson@gmail.com>
2023-12-31 20:57:35 +0100

    Some textual changes

--- app/makeIso.m
+++ app/makeIso.m
@@ -9,13 +9,13 @@ function [ tension, pcap, rrlaplace, zzlaplace ] = makeIso(zz_in,rr_in,psi_in,di
     z = zz_in;
     d = diffmat_in;
 
-    g_useP = 0; glob_p = 0;
+    g_useP = 0; p_measured = 0;
 
     N = length(r);
     C = 1;
     Gam = 10;
     if g_useP
-        P = glob_p;
+        P = p_measured;
     else
         P = Gam/2;
     end
@@ -97,44 +97,44 @@ function [ A, b] = matrix_iso(~, P, Gam, d,~,C,r,z,psi)
     % r-psi identity
     A11 = C*d;
     A13 = diag(sin(psi));
-    A18 = [d*r, zeros(N,num_auxvar-1)];
+    A14 = [d*r, zeros(N,num_auxvar-1)];
     b1 = cos(psi)-C*d*r;
 
     % boundary condition r(1) = 0
     A11(1,:) = IDL;
     A13(1,:) = ZL;
-    A18(1,:) = zeros(1,num_auxvar);
+    A14(1,:) = zeros(1,num_auxvar);
     b1(1) = -r(1);
 
     % z-psi identity
     A22 = C*d;
     A23 = diag(-cos(psi));
-    A28 = [d*z, zeros(N,num_auxvar-1)];
+    A24 = [d*z, zeros(N,num_auxvar-1)];
     b2 = sin(psi)-C*d*z;
 
     % boundary condition z(end) =0
     A22(end,:) = fliplr(IDL);
     A23(end,:) = ZL;
-    A28(end,:) = zeros(1,num_auxvar);
+    A24(end,:) = zeros(1,num_auxvar);
     b2(end) = -z(end);
 
     A31 = diag(-sin(psi)./r.^2);
     A32 = eye(N);
     A33 = C*Gam*d+diag(Gam*cos(psi)./r);
-    A38 = [Gam*d*psi,d*psi+sin(psi)./r, -ones(N,1) , zeros(N,num_auxvar-3)];
+    A34 = [Gam*d*psi,d*psi+sin(psi)./r, -ones(N,1) , zeros(N,num_auxvar-3)];
     b3 = -z+P-C*Gam*(d*psi)-Gam*sin(psi)./r;
 
     A31(1,:) = ZL;
     A32(1,:) = ZL;
     A33(1,:) = IDL;
-    A38(1,:) = zeros(1,num_auxvar);
+    A34(1,:) = zeros(1,num_auxvar);
     b3(1) = -psi(1);
 
     % Build small matrix, recheck.
 
-    %A = [[A11, Z, A13,A18];[Z, A22, A23,A28];[A31, A32, A33,A38];[fliplr(IDL), ZL,ZL,zeros(1,num_auxvar)]];
+    %A = [[A11, Z, A13,A14];[Z, A22, A23,A24];[A31, A32, A33,A34];[fliplr(IDL), ZL,ZL,zeros(1,num_auxvar)]];
     %b = [b1;b2;b3;rn-r(end)];
-    A = [[A11, Z, A13, A18(:,2:end)];[Z, A22, A23, A28(:,2:end)];[A31, A32, A33, A38(:,2:end)]];
+    A = [[A11, Z, A13, A14(:,2:end)];[Z, A22, A23, A24(:,2:end)];[A31, A32, A33, A34(:,2:end)]];
     b = [b1;b2;b3];
     
 end
