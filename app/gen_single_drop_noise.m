10f75973c6968c3e564bb22150d746d9f697d3d8
NickJaensson <n.jaensson@gmail.com>
2023-12-31 08:18:16 +0100

    Initial commit for inverse problem

---
+++ app/gen_single_drop_noise.m
@@ -0,0 +1,102 @@
+
+close all; clear
+
+% physical parameters
+params_phys.sigma = 4;        % surface tension
+params_phys.grav = 1.0;       % gravitational acceleration
+params_phys.rneedle = 1.0;    % radius of the needle
+params_phys.volume0 = 16;     % prescribed volume
+params_phys.deltarho = 1.0;   % density difference
+
+% numerical parameters
+params_num.N = 40;          % resolution of the discretization for calculation
+params_num.Nplot = 80;      % resolution of the discretization for plotting
+params_num.Ncheb = 10;      % number of Chebyshev to describe the shape
+params_num.alpha = 1;       % relaxation parameter in the Newton-Raphson scheme
+params_num.eps = 1e-12;     % convergence critertion: rms(u) < eps
+params_num.maxiter = 100;   % maximum number of iteration steps
+params.cheb_eps = 1e-2;     % error for describing the shape
+
+% solve the Young-Laplace equation for the given parameters
+[vars_sol,vars_num] = solve_forward_young_laplace(params_phys, params_num);
+
+% calculate the volume and the area
+volume = pi*vars_num.ws*(vars_sol.r.^2.*sin(vars_sol.psi));
+area = pi*2*vars_num.ws*(vars_sol.r);
+
+disp(['volume = ', num2str(volume,15)]);
+disp(['area = ', num2str(area,15)]);
+disp(['pressure = ', num2str(vars_sol.p0,15)]);
+
+% determine the normal vectors
+normals(:,1) = vars_num.Ds*vars_sol.z; % r-component of the normals
+normals(:,2) = -vars_num.Ds*vars_sol.r; % z-component of the normals
+for i=1:size(normals,2)
+    normals(i,:) = normals(i,:)/norm(normals(i,:));
+end
+
+% interpolate the numerical solutions on a finer grid. 
+% NOTE: the "right" way to interpolate is to fit a higher-orde polynomial 
+% though all the points (see book of Trefethen on Spectral Methods in 
+% Matlab, page  63). For plotting purposes we use a simpler interpolation 
+ss = linspace(vars_num.s(1),vars_num.s(end),params_num.Nplot)';
+rr = interp1(vars_num.s,vars_sol.r,ss,'pchip');
+zz = interp1(vars_num.s,vars_sol.z,ss,'pchip');
+
+nnormals(:,1) = interp1(vars_num.s,normals(:,1),ss,'pchip');
+nnormals(:,2) = interp1(vars_num.s,normals(:,2),ss,'pchip');
+for i=1:size(nnormals,2)
+    nnormals(i,:) = nnormals(i,:)/norm(nnormals(i,:));
+end
+
+% plot the shape of the drop on the plotting grid
+figure; hold on
+scatter(rr',zz','b');
+plot(rr',zz','b');
+set(gca,'DataAspectRatio',[1 1 1])
+quiver(rr,zz,nnormals(:,1),nnormals(:,2));
+
+% add noise to the data points
+sigma_noise = 0.001*params_phys.rneedle;
+tmp=normrnd(0,sigma_noise,[params_num.Nplot,1]);
+for i=1:params_num.Nplot
+    rr_noise(i) = rr(i) + tmp(i)*nnormals(i,1);
+    zz_noise(i) = zz(i) + tmp(i)*nnormals(i,2);
+end
+figure; hold on
+scatter(rr_noise',zz_noise','b');
+plot(rr',zz','r');
+set(gca,'DataAspectRatio',[1 1 1])
+
+% get continuous s around full shape
+ds = sqrt((zz_noise(2:end)-zz_noise(1:end-1)).^2+(rr_noise(2:end)-rr_noise(1:end-1)).^2);
+ss_noise = zeros(length(rr_noise),1);
+for i=1:length(ds)
+    ss_noise(1+i) = sum(ds(1:i));
+end
+
+% find smallest stepsize and interpolate on smaller grid
+dsmin = min(diff(ss_noise));
+ssb = linspace(0,ss_noise(end),ceil(0.5*ss_noise(end)/dsmin));
+rrb = interp1(ss_noise,rr_noise,ssb,'spline');
+zzb = interp1(ss_noise,zz_noise,ssb,'spline');
+
+% fit the points using Chebyshev polynomials
+fr = chebfun(rrb',[ssb(1),ssb(end)],'equi','eps',params.cheb_eps);
+fz = chebfun(zzb',[ssb(1),ssb(end)],'equi','eps',params.cheb_eps);
+figure; plot(fr); hold on; scatter(ssb,rrb);
+xlabel('s','FontSize',24); ylabel('r','FontSize',24);
+figure; plot(fz); hold on; scatter(ssb,zzb);
+xlabel('s','FontSize',24); ylabel('z','FontSize',24);
+
+vars_num_fit = numerical_grid(params_num,[0,ssb(end)]);
+
+rrfit_nag = gridsample(fr,params_num.N,[0,ssb(end)]);
+zzfit_nag = gridsample(fz,params_num.N,[0,ssb(end)]);
+psifit_nag = atan2(vars_num_fit.D*zzfit_nag,vars_num_fit.D*rrfit_nag);
+
+% calculate the best fitting Laplace shape
+[st,press,rrlaplace,zzlaplace] = ...
+    makeIso(zzfit_nag,rrfit_nag,psifit_nag,vars_num_fit.D);
+
+disp(['estimated surface tension = ',num2str(st,5)]);
---
+++ app/makeIso.m
@@ -0,0 +1,141 @@
+function [ tension, pcap, rrlaplace, zzlaplace ] = makeIso(zz_in,rr_in,psi_in,diffmat_in)
+    % makeIso(toplot) = [tension, pstat] fits the shape functions to surface 
+    % tension and pressure. RMS fitting of R using the Schur complement.
+    % toplot specifies if the result is to be plotted.
+
+    % assign some local variables
+    psi = psi_in;
+    r = rr_in;
+    z = zz_in;
+    d = diffmat_in;
+
+    g_useP = 0; glob_p = 0;
+
+    N = length(r);
+    C = 1;
+    Gam = 10;
+    if g_useP
+        P = glob_p;
+    else
+        P = Gam/2;
+    end
+    alpha = 0.25;
+    iter = 1;u=1;
+    rmsu = 1e3;
+
+    while rms(u) > 1e-9
+        
+        iter = iter+1;
+        
+        if iter > 100
+            error('Iteration did not converge!')
+        end  
+
+        % create matrix
+        [tA, tb] =  matrix_iso(0,P,Gam,d,0,C,r,z,psi);
+
+        % pressure solved or prescribed
+        if g_useP
+            A2 = tA(:,end-1);
+        else
+            A2 = tA(:,end-1:end);
+        end
+
+         % Schur Element
+        A1 = tA(:,1:end-2);
+
+        A3 = [diag(rr_in-r), zeros(1*N,2*N)];
+
+        IA1 = inv(A1);
+        SchurA = A3*(IA1*A2);
+        b = A3*(IA1*tb)-(rr_in-r).^2;
+        
+        if ( iter==2 ); warning('off', 'MATLAB:rankDeficientMatrix'); end
+        u2 = SchurA\b;
+        if ( iter==2 ); warning('on', 'MATLAB:rankDeficientMatrix'); end
+
+        u1 = IA1*(tb-A2*u2);
+        u = [u1;u2];
+
+        % update variables
+        r = r+alpha*u(1:N);
+        z = z+alpha*u(N+1:2*N);
+        psi = psi+alpha*u(2*N+1:3*N);
+        Gam = Gam+alpha*u(3*N+1);
+        if ~g_useP
+            P = P+alpha*u(end);
+        end
+
+        fprintf('iter %d: rms(u) = %d\n',iter,rms(u));
+
+        rmsb = rms(tb);
+    end
+
+    rrlaplace = r;
+    zzlaplace = z;
+    tension = Gam;
+    pcap = P;
+    
+end
+
+function [ A, b] = matrix_iso(~, P, Gam, d,~,C,r,z,psi)
+
+    % matrix and rhs for isotropic interface,
+    % its unknowns are: Gam, P.
+
+    % Final check uses this routine in all functions for drop create and detect
+
+    num_auxvar = 3;
+    N = length(r);
+
+    % full problem in r,z and psi
+    % zero matrix, zero line and identity line array
+    Z = zeros(N);
+    IDL = [1, zeros(1,N-1)];
+    ZL = zeros(1,N);
+
+    % r-psi identity
+    A11 = C*d;
+    A13 = diag(sin(psi));
+    A18 = [d*r, zeros(N,num_auxvar-1)];
+    b1 = cos(psi)-C*d*r;
+
+    % boundary condition r(1) = 0
+    A11(1,:) = IDL;
+    A13(1,:) = ZL;
+    A18(1,:) = zeros(1,num_auxvar);
+    b1(1) = -r(1);
+
+    % z-psi identity
+    A22 = C*d;
+    A23 = diag(-cos(psi));
+    A28 = [d*z, zeros(N,num_auxvar-1)];
+    b2 = sin(psi)-C*d*z;
+
+    % boundary condition z(end) =0
+    A22(end,:) = fliplr(IDL);
+    A23(end,:) = ZL;
+    A28(end,:) = zeros(1,num_auxvar);
+    b2(end) = -z(end);
+
+    A31 = diag(-sin(psi)./r.^2);
+    A32 = eye(N);
+    A33 = C*Gam*d+diag(Gam*cos(psi)./r);
+    A38 = [Gam*d*psi,d*psi+sin(psi)./r, -ones(N,1) , zeros(N,num_auxvar-3)];
+    b3 = -z+P-C*Gam*(d*psi)-Gam*sin(psi)./r;
+
+    A31(1,:) = ZL;
+    A32(1,:) = ZL;
+    A33(1,:) = IDL;
+    A38(1,:) = zeros(1,num_auxvar);
+    b3(1) = -psi(1);
+
+    % Build small matrix, recheck.
+
+    %A = [[A11, Z, A13,A18];[Z, A22, A23,A28];[A31, A32, A33,A38];[fliplr(IDL), ZL,ZL,zeros(1,num_auxvar)]];
+    %b = [b1;b2;b3;rn-r(end)];
+    A = [[A11, Z, A13, A18(:,2:end)];[Z, A22, A23, A28(:,2:end)];[A31, A32, A33, A38(:,2:end)]];
+    b = [b1;b2;b3];
+    
+end
+
