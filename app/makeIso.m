c31be89f29112e478c342b62118e4809b066f131
NickJaensson <n.jaensson@gmail.com>
2023-12-31 22:08:59 +0100

    Update makeIso.m

--- app/makeIso.m
+++ app/makeIso.m
@@ -96,43 +96,43 @@ function [ A, b] = matrix_iso(~, P, Gam, d,~,C,r,z,psi)
     % determine r from psi
     A11 = C*d; % N x N
     A13 = diag(sin(psi)); % N x N
-    A14 = [zeros(N,2)]; % N x 2
+    A145 = [zeros(N,2)]; % N x 2
     b1 = cos(psi)-C*d*r;  % N x 1
 
     % boundary condition r(1) = 0
     A11(1,:) = IDL; 
     A13(1,:) = ZL;
-    A14(1,:) = zeros(1,2);
+    A145(1,:) = zeros(1,2);
     b1(1) = -r(1);
 
     % determine z from psi
     A22 = C*d;  % N x N
     A23 = diag(-cos(psi)); % N x N
-    A24 = [zeros(N,2)]; % N x 2
+    A245 = [zeros(N,2)]; % N x 2
     b2 = sin(psi)-C*d*z; % N x 1
 
     % boundary condition z(end) =0
     A22(end,:) = fliplr(IDL);
     A23(end,:) = ZL;
-    A24(end,:) = zeros(1,2);
+    A245(end,:) = zeros(1,2);
     b2(end) = -z(end);
 
     % determine psi from Laplace law
     A31 = diag(-sin(psi)./r.^2); % N x N
     A32 = eye(N); % N x N
     A33 = C*Gam*d+diag(Gam*cos(psi)./r); % N x N
-    A34 = [d*psi+sin(psi)./r, -ones(N,1)]; % N x 2
+    A345 = [d*psi+sin(psi)./r, -ones(N,1)]; % N x 2
     b3 = -z+P-C*Gam*(d*psi)-Gam*sin(psi)./r; % N x 1
 
     % boundary condition phi(0) = 0
     A31(1,:) = ZL;
     A32(1,:) = ZL;
     A33(1,:) = IDL;
-    A34(1,:) = zeros(1,2);
+    A345(1,:) = zeros(1,2);
     b3(1) = -psi(1);
 
     % build complete matrix and RHS
-    A = [[A11, Z, A13, A14];[Z, A22, A23, A24];[A31, A32, A33, A34]];
+    A = [[A11, Z, A13, A145];[Z, A22, A23, A245];[A31, A32, A33, A345]];
     b = [b1;b2;b3];
     
 end
