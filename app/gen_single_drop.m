b3092cbd16c80201ee43d1c05513b0cf3d0214c0
NickJaensson <n.jaensson@gmail.com>
2024-01-01 21:18:34 +0100

    Add spaces

--- app/gen_single_drop.m
+++ app/gen_single_drop.m
@@ -2,11 +2,11 @@
 close all; clear
 
 % physical parameters
-params_phys.sigma = 4;        % surface tension
-params_phys.grav = 1.2;       % gravitational acceleration
-params_phys.rneedle = 1.4;    % radius of the needle
-params_phys.volume0 = 16;     % prescribed volume
-params_phys.deltarho = 1.1;   % density difference
+params_phys.sigma = 4;      % surface tension
+params_phys.grav = 1.2;     % gravitational acceleration
+params_phys.rneedle = 1.4;  % radius of the needle
+params_phys.volume0 = 16;   % prescribed volume
+params_phys.deltarho = 1.1; % density difference
 
 % numerical parameters
 params_num.N = 40;          % grid points for calculation
@@ -18,20 +18,20 @@ params_num.maxiter = 100;   % maximum number of iteration steps
 params_phys.Wo = params_phys.deltarho*params_phys.grav*...
     params_phys.volume0/(2*pi*params_phys.sigma*params_phys.rneedle);
 
-shape_guess = guess_shape(params_phys,1000);
+shape_guess = guess_shape(params_phys, 1000);
 
-vars_num = numerical_grid(params_num,[0,shape_guess.s(end)]);
+vars_num = numerical_grid(params_num, [0,shape_guess.s(end)]);
 
 vars_sol = solve_forward_young_laplace(params_phys, params_num, ...
                                        shape_guess, vars_num);
 
-vars_num = update_numerical_grid(vars_sol,vars_num,0);
+vars_num = update_numerical_grid(vars_sol, vars_num, 0);
 
-[volume,area] = calculate_volume_area(vars_sol,vars_num,1);
+[volume,area] = calculate_volume_area(vars_sol, vars_num, 1);
 
 [s_plot,r_plot,z_plot] = interpolate_solutions(vars_sol, vars_num, ...
                                                params_num);
 
-plot_shape(z_plot,r_plot);
+plot_shape(z_plot, r_plot);
 
-[kappas,kappap] = find_curvature(vars_sol,vars_num,1);
+[kappas,kappap] = find_curvature(vars_sol, vars_num,1);
