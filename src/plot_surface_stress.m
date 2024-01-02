54a6266d735334dc8e9faf2ac8bd69e8c1a445a0
NickJaensson <n.jaensson@gmail.com>
2024-01-02 16:36:08 +0100

    Make sure lines have correct color

--- src/plot_surface_stress.m
+++ src/plot_surface_stress.m
@@ -2,11 +2,11 @@ function [] =     plot_surface_stress(s, sigmas, sigmap, fig_num)
 
     % plot the surface stresses
     figure(fig_num); hold on
-    plot(s,sigmas,'LineWidth',2); hold on
-    plot(s,sigmap,'LineWidth',2);
+    h(1) = plot(s,sigmas,'LineWidth',2); hold on
+    h(2) = plot(s,sigmap,'LineWidth',2);
     xlabel('s','FontSize',32);
     ylabel('\sigma','FontSize',32);
-    legend('\sigma_s','\sigma_\phi','FontSize',24,'Location','northwest');
+    legend(h,'\sigma_s','\sigma_\phi','FontSize',24,'Location','northwest');
     xlim([0,s(end)])
     ax = gca; ax.FontSize = 24;
 
