050da48f79db25630ad91c90941c56314d0b19ab
NickJaensson <n.jaensson@gmail.com>
2024-01-02 14:31:11 +0100

    Bug fix

--- src/get_normals.m
+++ src/get_normals.m
@@ -3,7 +3,7 @@ function [normals] = get_normals(vars_sol, vars_num)
     % determine the normal vectors
     normals(:,1) = vars_num.Ds*vars_sol.z; % r-component of the normals
     normals(:,2) = -vars_num.Ds*vars_sol.r; % z-component of the normals
-    for i=1:size(normals,2)
+    for i=1:size(normals,1)
         normals(i,:) = normals(i,:)/norm(normals(i,:));
     end
 
