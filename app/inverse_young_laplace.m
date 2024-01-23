6b4bf8b26b3226cb50322dc0050c484935fbe1ce
NickJaensson <n.jaensson@gmail.com>
2024-01-23 14:14:31 +0100

    Add file for inverse Young-Laplace (simple)

---
+++ app/inverse_young_laplace.m
@@ -0,0 +1,29 @@
+
+gen_single_drop; 
+
+close all
+
+params_num.sigma_guess = 10;  % guess for interfacial tension value
+params_num.p0_guess = 5;      % guess for pressure
+params_num.eps_inv = 1e-9;    % convergence critertion forward: rms(u) < eps
+params_num.maxiter_inv = 1000; % maximum number of iteration steps inverse
+params_num.alpha = 0.5;       % relaxation parameter in inverse problem
+
+% this step is needed since the solve_inverse_young_laplce routine assumes
+% a value of C=1
+vars_num = numerical_grid(params_num,[0,vars_num.s(end)]);
+dummy.C = 1;
+vars_num = update_numerical_grid(dummy, vars_num, false);
+
+% calculate the best fitting Laplace shape
+[st,press,rrlaplace,zzlaplace] = solve_inverse_young_laplace(vars_sol.z, ...
+    vars_sol.r, vars_sol.psi, params_phys, params_num, vars_num);
+
+disp(['estimated surface tension = ',num2str(st,12)]);
+
+
+% the following code can be used to test the CMD implementation
+[sigmas, sigmap] = makeCMD(params_phys, vars_sol.psi, vars_sol.r, ...
+                           vars_sol.z, vars_num, vars_sol.p0);
+
+plot_surface_stress(vars_num.s, sigmas, sigmap, 3);
