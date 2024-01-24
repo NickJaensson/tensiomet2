c5b892cf24930cda7e21e86bff16c1e4751d0b05
NickJaensson <n.jaensson@gmail.com>
2024-01-24 21:38:30 +0100

    Move all code into inverse problem files

--- app/inverse_young_laplace.m
+++ app/inverse_young_laplace.m
@@ -4,21 +4,54 @@ gen_single_drop;
 % close all
 
 % numerical parameters for inverse problem
+params_num.eps_cheb = 1e-3;   % error for describing the shape
+params_num.eps_inv = 1e-4;    % convergence critertion inverse problem
 params_num.sigma_guess = 10;  % guess for interfacial tension value
 params_num.p0_guess = 5;      % guess for pressure
-params_num.eps_inv = 1e-4;    % convergence critertion forward: rms(u) < eps
-params_num.maxiter_inv = 100; % maximum number of iteration steps inverse
 params_num.alpha = 1.0;       % relaxation parameter in inverse problem
+params_num.maxiter_inv = 100; % maximum number of iteration steps inverse
+
+% number of points for the synthetic droplet shape (total amount of points
+% will be 2*Nsample-1
+Nsample = 80;
+
+% noise level
+sigma_noise = 0.01*params_phys.rneedle;
+rng(1); % set seed for reproducibility
 
 
-% calculate the best fitting Laplace shape
-[st,press,rrlaplace,zzlaplace] = solve_inverse_young_laplace(vars_sol, ...
-    params_phys, params_num, vars_num);
+% % run the inverse problem on the numerical problem (best case scenario)
+% [st,press,rrlaplace,zzlaplace] = solve_inverse_young_laplace ( ...
+%     vars_sol, params_phys, params_num, vars_num);
+% 
+% disp(['estimated surface tension = ',num2str(st,12)]);
+% 
+% %  run CMD on the numerical problem (best case scenario)
+% [sigmas, sigmap] = makeCMD(params_phys, vars_sol, vars_num);
+% 
+% plot_surface_stress(vars_num.s, sigmas, sigmap, 3);
+
+
+% generate uniform data points with noise
+vars_sol.normals = get_normals(vars_sol, vars_num);
+[rr_noise,zz_noise] = ...
+    generate_noisy_shape(vars_sol,vars_num,Nsample,sigma_noise);
+
+
+% fit the noisy shape with Cheby polynomials
+[vars_sol_fit,vars_num_fit] = ...
+    fit_shape_with_chebfun(rr_noise,zz_noise,params_num);
+
+
+% perform inverse Young-Laplace problem
+[st,press,rrlaplace,zzlaplace] = solve_inverse_young_laplace ( ...
+    vars_sol_fit, params_phys, params_num, vars_num_fit);
 
 disp(['estimated surface tension = ',num2str(st,12)]);
 
 
-% the following code can be used to test the CMD implementation
-[sigmas, sigmap] = makeCMD(params_phys, vars_sol, vars_num);
+plot_shape(rr_noise, zz_noise, 3);
+plot_shape(vars_sol_fit.r, vars_sol_fit.z, 3);
+plot_shape(rrlaplace, zzlaplace, 3);
 
-plot_surface_stress(vars_num.s, sigmas, sigmap, 3);
+legend('noisy data', 'Cheby fit', 'Laplace fit');
--- app/inverse_young_laplace_elastic.m
+++ app/inverse_young_laplace_elastic.m
@@ -4,21 +4,78 @@ gen_single_drop_elastic;
 % close all
 
 % numerical parameters for inverse problem
-params_num.eps_cheb = 1e-3;   % error for describing the shape
-params_num.eps_inv = 1e-3;    % convergence critertion inverse problem
+params_num.eps_cheb = 1e-4;   % error for describing the shape
+params_num.eps_inv = 1e-5;    % convergence critertion inverse problem
 params_num.sigma_guess = 10;  % guess for interfacial tension value
 params_num.p0_guess = 5;      % guess for pressure
 params_num.alpha = 1.0;       % relaxation parameter in inverse problem
-params_num.maxiter_inv = 100; % maximum number of iteration steps inverse
+params_num.maxiter_inv = 500; % maximum number of iteration steps inverse
+
+% number of points for the synthetic droplet shape (total amount of points
+% will be 2*Nsample-1
+Nsample = 80;
+
+% noise level
+sigma_noise = 1e-4*params_phys.rneedle;
+rng(1); % set seed for reproducibility
 
 
-% the following code can be used to test the CMD implementation
-[sigmas, sigmap] = makeCMD(params_phys, vars_sol, vars_num);
+% %  run CMD on the numerical problem (best case scenario)
+% [sigmas, sigmap] = makeCMD(params_phys, vars_sol, vars_num);
+% 
+% plot_surface_stress(vars_num.s, sigmas, sigmap, 2);
+% 
+% % run the inverse problem on the numerical problem (best case scenario)
+% 
+% [moduliS, lambda_s, lambda_r]  = makeSFE(params_phys.strainmeasure,...
+%     vars_sol_ref, vars_num_ref, vars_sol, vars_num,params_num);
+% 
+% errorG = abs(moduliS(1)-params_phys.Gmod)/params_phys.Gmod;
+% errorK = abs(moduliS(2)-params_phys.Kmod)/params_phys.Kmod;
+% 
+% disp(['Error in G = ', num2str(errorG*100,4), ' %']);
+% disp(['Error in K = ', num2str(errorK*100,4), ' %']);
+% 
+% plot_surface_strain(vars_num.s, lambda_s, lambda_r, 3);
 
-plot_surface_stress(vars_num.s, sigmas, sigmap, 2);
 
+% generate uniform data points with noise
+vars_sol_ref.normals = get_normals(vars_sol_ref, vars_num_ref);
+[rr_noise_ref,zz_noise_ref] = ...
+    generate_noisy_shape(vars_sol_ref,vars_num_ref,Nsample,sigma_noise);
+
+vars_sol.normals = get_normals(vars_sol, vars_num);
+[rr_noise,zz_noise] = ...
+    generate_noisy_shape(vars_sol,vars_num,Nsample,sigma_noise);
+
+
+% fit the noisy shape with Cheby polynomials
+[vars_sol_ref_fit,vars_num_ref_fit] = ...
+    fit_shape_with_chebfun(rr_noise_ref,zz_noise_ref,params_num);
+vars_sol_ref_fit.p0 = vars_sol_ref.p0;
+
+[vars_sol_fit,vars_num_fit] = ...
+    fit_shape_with_chebfun(rr_noise,zz_noise,params_num);
+vars_sol_fit.p0 = vars_sol.p0;
+
+
+% perform CMD to find the surface stresses
+[vars_sol_ref_fit.sigmas, vars_sol_ref_fit.sigmap] = ...
+    makeCMD(params_phys, vars_sol_ref_fit, vars_num_ref_fit);
+
+[vars_sol_fit.sigmas, vars_sol_fit.sigmap] = ...
+    makeCMD(params_phys, vars_sol_fit, vars_num_fit);
+
+plot_surface_stress(vars_num_ref_fit.s, vars_sol_ref_fit.sigmas, ...
+    vars_sol_ref_fit.sigmap, 2);
+plot_surface_stress(vars_num_fit.s, vars_sol_fit.sigmas, ...
+    vars_sol_fit.sigmap, 2);
+
+
+% perform SFE to find the moduli and strains
 [moduliS, lambda_s, lambda_r]  = makeSFE(params_phys.strainmeasure,...
-    vars_sol_ref, vars_num_ref, vars_sol, vars_num,params_num);
+    vars_sol_ref_fit, vars_num_ref_fit, vars_sol_fit, vars_num_fit, ...
+    params_num);
 
 errorG = abs(moduliS(1)-params_phys.Gmod)/params_phys.Gmod;
 errorK = abs(moduliS(2)-params_phys.Kmod)/params_phys.Kmod;
@@ -26,4 +83,10 @@ errorK = abs(moduliS(2)-params_phys.Kmod)/params_phys.Kmod;
 disp(['Error in G = ', num2str(errorG*100,4), ' %']);
 disp(['Error in K = ', num2str(errorK*100,4), ' %']);
 
-plot_surface_strain(vars_num.s, lambda_s, lambda_r, 3);
+plot_surface_strain(vars_num_fit.s, lambda_s, lambda_r, 3);
+
+
+plot_shape(rr_noise_ref, zz_noise_ref, 5);
+plot_shape(vars_sol_ref_fit.r, vars_sol_ref_fit.z, 5);
+plot_shape(rr_noise, zz_noise, 5);
+plot_shape(vars_sol_fit.r, vars_sol_fit.z, 5);
