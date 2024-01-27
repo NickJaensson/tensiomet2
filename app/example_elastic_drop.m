f8574e1db76ef1d18e56518c381a067f26c66d74
NickJaensson <n.jaensson@gmail.com>
2024-01-27 11:04:17 -0600

    Update example_elastic_drop.m

--- app/example_elastic_drop.m
+++ app/example_elastic_drop.m
@@ -1,5 +1,5 @@
-% calculate the Laplace shape for a given surface tension and given
-% pressure/volume/area
+% calculate the shape for a given elastic interface using surface or 
+% volume compressions/dilations
 
 close all; clear
 
@@ -7,10 +7,10 @@ example_parameters; % load the parameters values
 
 [vars_num_ref, vars_sol_ref] = gen_single_drop(params_phys, params_num);
 
-[vars_num,vars_sol] = gen_single_drop_elastic(params_phys, params_num,...
-    vars_num_ref, vars_sol_ref);
+[vars_num, vars_sol] = gen_single_drop_elastic(params_phys, ...
+    params_num, vars_num_ref, vars_sol_ref);
 
-[volume,area] = calculate_volume_area(vars_sol, vars_num, true);
+[volume, area] = calculate_volume_area(vars_sol, vars_num, true);
 
 plot_shape(vars_sol.r, vars_sol.z, 1);
 
@@ -18,6 +18,6 @@ plot_surface_stress(vars_num.s, vars_sol.sigmas, vars_sol.sigmap, 2);
 
 plot_surface_strain(vars_num.s, vars_sol.lams, vars_sol.lamp, 3);
 
-[kappas,kappap] = find_curvature(vars_sol, vars_num);
+[kappas, kappap] = find_curvature(vars_sol, vars_num);
 
 plot_curvature(vars_sol.z, kappas, kappap, 4);
