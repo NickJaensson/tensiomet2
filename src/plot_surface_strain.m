6b4eccf05e2ea1b982def2e00744e7377b9af612
NickJaensson <n.jaensson@gmail.com>
2025-04-16 19:46:33 +0200

    Optional argument for line style

--- src/plot_surface_strain.m
+++ src/plot_surface_strain.m
@@ -1,4 +1,4 @@
-function [] = plot_surface_strain(s, lams, lamp, fig_num)
+function [] = plot_surface_strain(s, lams, lamp, fig_num, style)
     % PLOT_SURFACE_STRAIN Plots the surface strain components.
     %
     % INPUTS:
@@ -6,11 +6,16 @@ function [] = plot_surface_strain(s, lams, lamp, fig_num)
     %   lams    - Meridional stretch ratio (\lambda_s).
     %   lamp    - Circumferential stretch ratio (\lambda_\phi).
     %   fig_num - Figure number for plotting.
+    %   style   - Optional: Line style for plotting (string, e.g., '--').
+
+    if nargin < 5
+        style = '-';
+    end
 
     % plot the surface strains
     figure(fig_num); hold on
-    h(1) = plot(s,lams,'LineWidth',2); hold on
-    h(2) = plot(s,lamp,'LineWidth',2);
+    h(1) = plot(s,lams,'LineWidth',2,'LineStyle',style); hold on
+    h(2) = plot(s,lamp,'LineWidth',2,'LineStyle',style);
     xlabel('s','FontSize',32);
     ylabel('\lambda','FontSize',32);
     legend(h,'\lambda_s','\lambda_\phi','FontSize',24,'Location','northwest');
--- src/plot_surface_stress.m
+++ src/plot_surface_stress.m
@@ -1,4 +1,4 @@
-function [] =     plot_surface_stress(s, sigmas, sigmap, fig_num)
+function [] = plot_surface_stress(s, sigmas, sigmap, fig_num, style)
     % PLOT_SURFACE_STRESS Plots the surface stress components.
     %
     % INPUTS:
@@ -6,11 +6,16 @@ function [] =     plot_surface_stress(s, sigmas, sigmap, fig_num)
     %   lams    - Meridional stretch ratio (\lambda_s).
     %   lamp    - Circumferential stretch ratio (\lambda_\phi).
     %   fig_num - Figure number for plotting.
+    %   style   - Optional: Line style for plotting (string, e.g., '--').
+
+    if nargin < 5
+        style = '-';
+    end
 
     % plot the surface stresses
     figure(fig_num); hold on
-    h(1) = plot(s,sigmas,'LineWidth',2); hold on
-    h(2) = plot(s,sigmap,'LineWidth',2);
+    h(1) = plot(s,sigmas,'LineWidth',2,'LineStyle',style); hold on
+    h(2) = plot(s,sigmap,'LineWidth',2,'LineStyle',style);
     xlabel('s','FontSize',32);
     ylabel('\sigma','FontSize',32);
     legend(h,'\sigma_s','\sigma_\phi','FontSize',24,'Location','northwest');
