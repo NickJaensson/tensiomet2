8564526eb44137143ff486b3bcd4132c27ea4da1
Nick Jaensson <n.jaensson@gmail.com>
2025-11-05 21:32:37 +0100

    Add Bo and nu to params_phys

--- examples/example_parameters/parameters_simple.m
+++ examples/example_parameters/parameters_simple.m
@@ -8,3 +8,10 @@ params_phys.deltarho = 1.1; % density difference
 % Worthington number (needed for initial shape guess)
 params_phys.Wo = params_phys.deltarho*params_phys.grav*...
     params_phys.volume0/(2*pi*params_phys.sigma*params_phys.rneedle);
+
+% Bond number (stored for convenience)
+params_phys.Bo = params_phys.deltarho*params_phys.grav*...
+    params_phys.rneedle^2/params_phys.sigma;
+
+% dimensionless volume (stored for convenience)
+params_phys.nu = params_phys.volume0 / params_phys.rneedle^3;
